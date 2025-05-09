8dcf946c3344823af074e669ce16340a3488e79f
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-05-09 13:05:03 +0100

    api model

--- userapis/models.py
+++ userapis/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+GENDER_CHOICES=(
+    ('M','Male'),
+    ('F','Female'),
+)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15,null=True,blank=True)
+    gender = models.CharField(max_length=6,choices=GENDER_CHOICES)
+    image = models.ImageField(upload_to='profile',null=True,blank=True,default="https://png.pngtree.com/png-vector/20190329/ourmid/pngtree-vector-avatar-icon-png-image_889567.jpg")
+    created = models.DateTimeField(auto_now_add=False)
+    def __str__(self):
+        return self.fullname
