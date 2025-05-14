12f84440dde785d11daab38dada30d0c56fc632a
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-05-14 13:00:49 +0100

    deploy

--- userapis/serializers.py
+++ userapis/serializers.py
@@ -1,1 +1,48 @@
 from rest_framework import serializers
+from . models import Profile
+from django.contrib.auth.models import User
+
+from . utils import SendMail
+
+# serialize django user
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= User
+        fields = ['username','email']
+
+# serialize profile
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= UserSerializer()
+        fields = ['fullname','gender','phone','image']
+
+# registration serializer
+class RegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+    email = serializers.EmailField(write_only=True)
+    class Meta:
+        model = Profile
+        fields = ['fullname','username','email','password1','password2','gender','phone','image']
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Password not match')
+        return data
+    def create(self, validated_data):
+        username = validated_data.pop('username')
+        email = validated_data.pop('email')
+        password = validated_data.pop('password1')
+        
+        user = User.objects.create_user(username=username,email=email,password=password)
+
+        profile = Profile.objects.create(
+            user=user,
+            fullname = validated_data['fullname'],
+            phone = validated_data['phone'],
+            gender = validated_data['gender'],
+            image = validated_data.get('image'),
+        )
+        SendMail(email,profile.fullname)
+        return profile
+    
--- userapis/views.py
+++ userapis/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from rest_framework import serializers,status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
+from . models import Profile
+from . serializers import RegistrationSerializer, UserSerializer
+
+class RegisterView(APIView):
+    def post(self,request):
+        try:
+            serializers = RegistrationSerializer(data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"Error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class LoginView(APIView):
+    def post(self,request):
+        try:
+            username = request.data.get('username')
+            password = request.data.get('password')
+            user = authenticate(username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return Response({"Message":"User logged in successfully!"}, status=status.HTTP_200_OK)
+            return Response({"Message":" Invalid username/password !"}, status=status.HTTP_400_BAD_REQUEST)
+            
+        except Exception as e:
+            return Response({"Error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class LogoutView(APIView):
+    def post(self,request):
+        try:
+            logout(request)
+            return Response({"Message":"User logged out successfully!"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"Error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class UserDashboardView(APIView):
+    def get(self,request):
+        try:
+            user = request.user.profile
+            print(user)
+            return Response({"Message":f"welcome {user.fullname}"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"Error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
